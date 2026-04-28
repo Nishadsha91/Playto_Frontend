71fc8bbfdfdf0570c9a5de62456931bfb1fd2b9c
Nishadsha91 <muthumattilnishad91@gmail.com>
2026-04-28 18:23:01 +0530

    updated url

--- playto_ui/src/services/api.jsx
+++ playto_ui/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://web-production-29b08d.up.railway.app/api/v1/",
+  baseURL: import.meta.env.VITE_API_URL || "https://playto-backend-5abs.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
